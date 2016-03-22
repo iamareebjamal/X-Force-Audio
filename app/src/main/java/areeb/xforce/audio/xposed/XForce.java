698467655a0f4818c4346c542c598869d4053f6f
iamareebjamal <jamal.areeb@gmail.com>
2016-03-22 20:51:41 +0530

    Added Theoretical Code

--- app/src/main/java/areeb/xforce/audio/xposed/XForce.java
+++ app/src/main/java/areeb/xforce/audio/xposed/XForce.java
@@ -1,18 +1,27 @@
 package areeb.xforce.audio.xposed;
 
+import android.media.AudioManager;
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
 public class XForce implements IXposedHookLoadPackage {
 
-    public static final String HOOK_CLASS = "android";
+    private static final String HOOK_PACKAGE = "android", HOOK_CLASS = "android.media.AudioManager";
+    private static final String HOOK_METHOD = "requestAudioFocus";
 
     @Override
     public void handleLoadPackage(LoadPackageParam loadPackageParam) throws Throwable {
-        if(!loadPackageParam.packageName.equals(HOOK_CLASS))
+        if (!loadPackageParam.packageName.equals(HOOK_PACKAGE))
             return;
 
-        XposedBridge.log("Entered Android");
+        XposedBridge.log("Entered Android\nTrying to hook method");
+        findAndHookMethod(HOOK_CLASS, loadPackageParam.classLoader, HOOK_METHOD,
+                AudioManager.OnAudioFocusChangeListener.class, int.class, int.class,
+                XC_MethodReplacement.returnConstant(AudioManager.AUDIOFOCUS_REQUEST_GRANTED));
+        XposedBridge.log("Bomb has been planted!");
     }
 }
